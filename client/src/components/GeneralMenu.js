3ceb3120fc3a8ef378e0c89559df42c6e05f8f4b
sendrarisk <cristianv129@hotmail.com>
2019-12-20 10:00:30 -0300

    el ultmo

--- client/src/components/GeneralMenu.js
+++ client/src/components/GeneralMenu.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { Link } from "react-router-dom";
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 import '../styles/GeneralMenu.css';
 
-const Menu = (props) => {
+const Menu = props => {
   const [dropdownOpen, setOpen] = useState(false);
 
   const toggle = () => setOpen(!dropdownOpen);
 
   return (
-    <ButtonDropdown isOpen={dropdownOpen} toggle={toggle} className="bottonDropdown">
-      <DropdownToggle className="dropdownToggleMenu" />
+    <ButtonDropdown
+      isOpen={dropdownOpen}
+      toggle={toggle}
+      className='bottonDropdown'
+    >
+      <DropdownToggle className='dropdownToggleMenu' />
       <DropdownMenu right>
         <DropdownItem>
-          <Link to="/" className="d-flex justify-content-center">
-            Option 1
+          <Link to='/cities' className='d-flex justify-content-center'>
+            cities
           </Link>
         </DropdownItem>
         <DropdownItem>
-          <Link to="/" className="d-flex justify-content-center">
-            Option 2
+          <Link to='/' className='d-flex justify-content-center'>
+            home
           </Link>
         </DropdownItem>
       </DropdownMenu>
